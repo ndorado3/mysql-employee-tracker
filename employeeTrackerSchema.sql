ca6e1ab1a82c24a955c153b00c204f87817b1e55
nadiadorado@gmail.com <ndorado@uncc.edu>
2021-05-05 20:12:57 -0400

    download 3 dependencies console.table, mysql and inquirer

---
+++ employeeTrackerSchema.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS employeeTracker_DB;
+CREATE DATABASE employeeTracker_DB;
+USE employeeTracker_DB;
+-- CreateS 3 tables: departments, role and employee
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR (30) NOT NULL,
+  PRIMARY KEY (id)
+);
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR (30) NOT NULL,
+  salary DECIMAL,
+  department_id INT PRIMARY KEY (id)
+);
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR (30) NOT NULL,
+  last_name VARCHAR (30) NOT NULL,
+  role_id INT,
+  manager_id INT NULL
+);
